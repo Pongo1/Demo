c53b9307f876c1f897c369688364fece0d3694c2
pongo1 <mrfimpong@gmail.com>
2019-10-08 15:50:49 +0400

    Updates

---
+++ precache-manifest.eef4c20250dad478d651ca5b2b9bc653.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "b06c243f534d9c5461d16528156cd5a8",
+    "url": "/Demo/static/media/openhand.b06c243f.cur"
+  },
+  {
+    "revision": "c5b5636327081ab73aa7",
+    "url": "/Demo/static/css/main.623756e7.chunk.css"
+  },
+  {
+    "revision": "4fea47160cdb7a422599",
+    "url": "/Demo/static/js/runtime~main.c50a1f49.js"
+  },
+  {
+    "revision": "ae19d4e10bc369ef48619127769cffe0",
+    "url": "/Demo/static/media/logo.ae19d4e1.svg"
+  },
+  {
+    "revision": "586366199f0c433ca8bb",
+    "url": "/Demo/static/js/2.77a8b011.chunk.js"
+  },
+  {
+    "revision": "ee45265d40afac56de2c738bf93daa84",
+    "url": "/Demo/static/media/logo.ee45265d.png"
+  },
+  {
+    "revision": "c9ce85b7b97604c9eace6de11fd95123",
+    "url": "/Demo/static/media/preloader.c9ce85b7.gif"
+  },
+  {
+    "revision": "0381f18808cf30db073365bc7dc65166",
+    "url": "/Demo/static/media/icon-select.0381f188.png"
+  },
+  {
+    "revision": "f92938639fa894a0e8ded1c3368abe98",
+    "url": "/Demo/static/media/fancybox_loading@2x.f9293863.gif"
+  },
+  {
+    "revision": "342c1ab1971f46711730c5ce3355a399",
+    "url": "/Demo/static/media/demo.342c1ab1.jpg"
+  },
+  {
+    "revision": "342c1ab1971f46711730c5ce3355a399",
+    "url": "/Demo/static/media/coloredbg.342c1ab1.jpg"
+  },
+  {
+    "revision": "10e8a5455c4522c48aa975eacd4f0023",
+    "url": "/Demo/static/media/flexslider-icon.10e8a545.svg"
+  },
+  {
+    "revision": "f8b92f66539473eea649c8514eb836a0",
+    "url": "/Demo/static/media/flexslider-icon.f8b92f66.woff"
+  },
+  {
+    "revision": "9c9cb7a6055043933ba68854f521af45",
+    "url": "/Demo/static/media/flexslider-icon.9c9cb7a6.eot"
+  },
+  {
+    "revision": "b4c9e5057989b9727a5df4e0a21af33c",
+    "url": "/Demo/static/media/flexslider-icon.b4c9e505.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Demo/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "c5b5636327081ab73aa7",
+    "url": "/Demo/static/js/main.15d3ba33.chunk.js"
+  },
+  {
+    "revision": "a8c874b93b3d848f39a71260c57e3863",
+    "url": "/Demo/static/media/closedhand.a8c874b9.cur"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Demo/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Demo/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "1142a254da20b59e7d10ac39e78c4fbf",
+    "url": "/Demo/static/media/revicons.1142a254.svg"
+  },
+  {
+    "revision": "c0d99a716110bb48fc975b1f006dc7ac",
+    "url": "/Demo/static/media/icomoon.c0d99a71.ttf"
+  },
+  {
+    "revision": "410ff0ac64f69d5da244f3335b17b30d",
+    "url": "/Demo/static/media/icomoon.410ff0ac.woff"
+  },
+  {
+    "revision": "61e75b90a4e1e9b620c593b86d59ef59",
+    "url": "/Demo/static/media/icomoon.61e75b90.eot"
+  },
+  {
+    "revision": "e380a832dceabf1803f3c5182747e7e9",
+    "url": "/Demo/static/media/loader.e380a832.gif"
+  },
+  {
+    "revision": "679106f20b33079c3777dee352027443",
+    "url": "/Demo/static/media/icomoon.679106f2.svg"
+  },
+  {
+    "revision": "db812d8a70a4e88e888744c1c9a27e89",
+    "url": "/Demo/static/media/fontawesome-webfont.db812d8a.woff2"
+  },
+  {
+    "revision": "32400f4e08932a94d8bfd2422702c446",
+    "url": "/Demo/static/media/fontawesome-webfont.32400f4e.eot"
+  },
+  {
+    "revision": "a35720c2fed2c7f043bc7e4ffb45e073",
+    "url": "/Demo/static/media/fontawesome-webfont.a35720c2.woff"
+  },
+  {
+    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
+    "url": "/Demo/static/media/fontawesome-webfont.a3de2170.ttf"
+  },
+  {
+    "revision": "f775f9cca88e21d45bebe185b27c0e5b",
+    "url": "/Demo/static/media/fontawesome-webfont.f775f9cc.svg"
+  },
+  {
+    "revision": "586366199f0c433ca8bb",
+    "url": "/Demo/static/css/2.719a4c74.chunk.css"
+  },
+  {
+    "revision": "d93ecd95a61181af3f96f633985bc765",
+    "url": "/Demo/index.html"
+  }
+];
